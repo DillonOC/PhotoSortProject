2ebf763b4d8fe136599a975729a47ffb32829a90
Dillon O'Connor <37981696+DillonOC@users.noreply.github.com>
2026-07-29 15:53:03 +0100

    DestinationPlanner basic implementation written
    Still needs testing

--- src/main/java/files/DestinationPlanner.java
+++ src/main/java/files/DestinationPlanner.java
@@ -1,19 +1,30 @@
 package files;
 
 import java.nio.file.Path;
+import java.util.Calendar;
 
 public class DestinationPlanner 
 {
 
-    public Path planSortedPhotoDestination(Path outputFolder)
+    public Path planSortedPhotoDestination(Path outputFolder, Calendar photoDate, Path photo)
     {
-        Path destination = outputFolder.resolve("test");
+        Integer year = photoDate.get(Calendar.YEAR);
+        Integer month = photoDate.get(Calendar.MONTH) + 1; // Calendar.MONTH is zero-based
+
+        Path destination = outputFolder.resolve(year.toString()).resolve(month.toString()) 
+                .resolve(photo.getFileName());
+        
         return destination;
     }
 
-    public Path planExactDuplicatePhotoDestination(Path outputFolder)
+    public Path planExactDuplicatePhotoDestination(Path outputFolder, Calendar photoDate, Path photo)
     {
-        Path destination = outputFolder.resolve("test");
+        Integer year = photoDate.get(Calendar.YEAR);
+        Integer month = photoDate.get(Calendar.MONTH) + 1; // Calendar.MONTH is zero-based
+
+        Path destination = outputFolder.resolve(year.toString()).resolve(month.toString())
+                .resolve("Duplicates").resolve(photo.getFileName());
+        
         return destination;
     }
     
