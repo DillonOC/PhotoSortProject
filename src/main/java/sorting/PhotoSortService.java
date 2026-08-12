05d77e61fb7c609404971b3cb8ecfc9f53549312
Dillon O'Connor <37981696+DillonOC@users.noreply.github.com>
2026-08-12 17:10:21 +0100

    PhotoSortVisitor created and plan moving forward decided

--- src/main/java/sorting/PhotoSortService.java
+++ src/main/java/sorting/PhotoSortService.java
@@ -1,11 +1,13 @@
 package sorting;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 public class PhotoSortService {
     
-    public void sortPhotos(Path source, Path ouput)
+    public void sortPhotos(Path source, Path ouput) throws Exception
     {
-        
+            //implementing file tree walk
     }
 }
