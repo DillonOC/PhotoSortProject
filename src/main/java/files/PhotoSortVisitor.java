f18f55cf2ce4b1e105e46fee982b5a151eb499ce
Dillon O'Connor <37981696+DillonOC@users.noreply.github.com>
2026-08-17 17:08:03 +0100

    Changed some implementation - WIP

--- src/main/java/files/PhotoSortVisitor.java
+++ src/main/java/files/PhotoSortVisitor.java
@@ -6,10 +6,17 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.io.FileWriter;
 import hashing.ContentHasher;
+import hashing.ContentHasherException;
 import detection.DuplicateDetector;
 
 public class PhotoSortVisitor extends SimpleFileVisitor<Path> {
+
+    DuplicateDetector detector = new DuplicateDetector(); 
+    private final Path outputFolder;
+
+    // NOTE: Add constructor with Path variable for outputFolder
 
     // Call content 
     @Override
@@ -17,30 +24,35 @@ public class PhotoSortVisitor extends SimpleFileVisitor<Path> {
                                    BasicFileAttributes attr) {
         if (attr.isRegularFile()) {
             ContentHasher hasher = new ContentHasher();
-            DuplicateDetector detector = new DuplicateDetector();
-            String hash = hasher.createContentHash(file);
+            String hash = null;
+            
+            try {
+                hash = hasher.createContentHash(file);
+            }
+            catch(hashing.ContentHasherException e) {
+                visitFileFailed(file, e);
+            }
 
+            //Add date extraction
+
+            DestinationPlanner planner = new DestinationPlanner();
+
+            Path duplicate = detector.detectDuplicate(hash, file);
+            if(duplicate == null) {
+                planner.planSortedPhotoDestination(outputFolder, );
+            }
         }
         return CONTINUE;
     }
 
-    // Print each directory visited.
-    @Override
-    public FileVisitResult postVisitDirectory(Path dir,
-                                          IOException exc) {
-        System.out.format("Directory: %s%n", dir);
-        return CONTINUE;
-    }
-
-    // If there is some error accessing
-    // the file, let the user know.
-    // If you don't override this method
-    // and an error occurs, an IOException 
-    // is thrown.
-    @Override
     public FileVisitResult visitFileFailed(Path file,
-                                       IOException exc) {
-        System.err.println(exc);
-        return CONTINUE;
+                                       ContentHasherException exc) {
+        try(FileWriter errorWriter = new FileWriter("failed_visits.txt", true)) {
+            errorWriter.write(file.toString() + System.lineSeparator());
+            System.out.println("Visit failed - file path written to failed_visits.txt");
+        } catch(IOException e) {
+            System.out.println("Visit failed - could not write to failed_vists.txt");
+        }
+        return FileVisitResult.CONTINUE;
     }
 }
